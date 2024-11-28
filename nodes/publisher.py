2ed9357fa7b7bad28905fef7d1fdf783a8bc8baa
midoriiiiii <keyi.mai@tum.de>
2024-11-28 02:14:06 -0800

    Add parameter support to publisher node and update launch file

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -2,10 +2,15 @@
 import rospy
 from std_msgs.msg import String
 
+
 rospy.init_node('publisher')
-rate = rospy.Rate(2)
+
+message = rospy.get_param('~message', 'Hello World!')
+
+rate = rospy.Rate(2)  
 pub = rospy.Publisher('/message', String, queue_size=10)
 
 while not rospy.is_shutdown():
-    pub.publish("Hello world!")
+    pub.publish(message) 
     rate.sleep()
+
